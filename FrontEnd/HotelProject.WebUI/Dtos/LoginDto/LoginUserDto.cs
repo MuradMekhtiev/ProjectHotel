15609817ed8cf0f4defc5150a5f280ef38575d2e
MuradMekhtiev <60578233+MuradMekhtiev@users.noreply.github.com>
2023-06-11 01:52:15 +0400

    Registration and Login pages added

---
+++ FrontEnd/HotelProject.WebUI/Dtos/LoginDto/LoginUserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelProject.WebUI.Dtos.LoginDto
+{
+    public class LoginUserDto
+    {
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Please fill out this field.")]
+        public string Username { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Please fill out this field.")]
+        public string Password { get; set; }
+    }
+}
---
+++ FrontEnd/HotelProject.WebUI/Dtos/RegisterDto/CreateNewUserDto.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.RegisterDto
+{
+    public class CreateNewUserDto
+    {
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Please fill out this field.")]
+        public string Name { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Please fill out this field.")]
+        public string Surname { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Please fill out this field.")]
+        public string Username { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Please fill out this field.")]
+        public string City { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Please fill out this field.")]
+        public string Email { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Please fill out this field.")]
+        public string Password { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Please fill out this field.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
