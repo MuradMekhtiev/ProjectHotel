4d2be59eff7a9cc08fec3ee4861f49ea9ab9c7d2
MuradMekhtiev <60578233+MuradMekhtiev@users.noreply.github.com>
2023-06-12 12:36:09 +0400

    User Interface added.

--- ApiConsume/HotelProject.WebApi/Program.cs
+++ ApiConsume/HotelProject.WebApi/Program.cs
@@ -24,6 +24,9 @@ builder.Services.AddScoped<IRoomService, RoomManager>();
 builder.Services.AddScoped<ISubscribeDal, EfSubscribeDal>();
 builder.Services.AddScoped<ISubscribeService, SubscribeManager>();
 
+builder.Services.AddScoped<IAboutDal, EfAboutDal>();
+builder.Services.AddScoped<IAboutService, AboutManager>();
+
 builder.Services.AddAutoMapper(typeof(Program));
 //builder.Services.AddScoped
 
