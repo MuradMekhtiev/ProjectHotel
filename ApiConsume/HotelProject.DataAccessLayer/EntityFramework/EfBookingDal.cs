573574ca3f06937efb8a40f68ce99b3fa5e7a483
MuradMekhtiev <60578233+MuradMekhtiev@users.noreply.github.com>
2023-06-13 12:21:16 +0400

    Booking funconality added

---
+++ ApiConsume/HotelProject.DataAccessLayer/EntityFramework/EfBookingDal.cs
@@ -0,0 +1,36 @@
+﻿using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrete;
+using HotelProject.DataAccessLayer.Repositories;
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.EntityFramework
+{
+    public class EfBookingDal : GenericRepository<Booking>, IBookingDal
+    {
+        public EfBookingDal(Context context) : base(context)
+        {
+
+        }
+
+        public void ApproveBookingStatus(Booking booking)
+        {
+            var context = new Context();
+            var values = context.Bookings.Where(x => x.BookingID == booking.BookingID).FirstOrDefault();
+            values.Status = "Approved";
+            context.SaveChanges();
+        }
+
+        public void ApproveBookingStatus2(int id)
+        {
+            var context = new Context();
+            var values = context.Bookings.Find(id);
+            values.Status = "Approved";
+            context.SaveChanges();
+        }
+    }
+}
---
+++ ApiConsume/HotelProject.WebApi/Controllers/BookingController.cs
@@ -0,0 +1,63 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        private readonly IBookingService _bookingService;
+
+        public BookingController(IBookingService bookingService)
+        {
+            _bookingService = bookingService;
+        }
+
+        [HttpGet]
+        public IActionResult BookingList()
+        {
+            var values = _bookingService.TGetList();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult AddBooking(Booking booking)
+        {
+            _bookingService.TInsert(booking);
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBooking(int id)
+        {
+            var values = _bookingService.TGetByID(id);
+            _bookingService.TDelete(values);
+            return Ok();
+        }
+        [HttpPut("UpdateBooking")]
+        public IActionResult UpdateBooking(Booking booking)
+        {
+            _bookingService.TUpdate(booking);
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetBooking(int id)
+        {
+            var value = _bookingService.TGetByID(id);
+            return Ok(value);
+        }
+        [HttpPut("ApproveReservation")]
+        public IActionResult ApproveReservation(Booking booking)
+        {
+            _bookingService.TApproveBookingStatus(booking);
+            return Ok();
+        }
+        [HttpPut("ApproveReservation2")]
+        public IActionResult ApproveReservation2(int id)
+        {
+            _bookingService.TApproveBookingStatus2(id);
+            return Ok();
+        }
+    }
+}
